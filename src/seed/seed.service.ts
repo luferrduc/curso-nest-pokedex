b36fc57f83c10daaca35dacfd7af01dd6811856b
luferrduc <lu.ferrando@duocuc.cl>
2024-10-22 22:15:22 -0300

    Add: Seed service pre tarea

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios
+
+  // https://pokeapi.co/api/v2/pokemon?limit=650
+  async executeSeed() {
+    const { data }= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/')
+      const nro: number = +segments[segments.length - 2]
+      console.log({ name, nro })
+    })
+
+
+    return data.results
+  }
+}
